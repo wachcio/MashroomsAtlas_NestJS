367c80a39327cdf7e58bde712ddf525ae951eee8
wachcio <wachcio@poczta.fm>
2020-11-29 15:05:21 +0100

    add types to musroomItem class

--- src/mushroom/mushroom-item.entity.ts
+++ src/mushroom/mushroom-item.entity.ts
@@ -1,7 +1,7 @@
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class MushroomItem extends BaseEntity {
+export class MushroomItem extends BaseEntity implements MushroomItem {
   @PrimaryGeneratedColumn('uuid')
   id: number;
 
