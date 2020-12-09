60d771b4afbfd8fa9c3c7fd0a261f515fb3166ed
wachcio <wachcio@poczta.fm>
2020-12-09 22:09:31 +0100

    user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,32 @@
-export class User {}
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { ItemInBasket } from '../basket/item-in-basket.entity';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    length: 255,
+  })
+  email: string;
+
+  @Column()
+  pwdHash: string;
+
+  @Column({
+    nullable: true,
+    default: null,
+  })
+  currentTokenId: string | null;
+
+  @OneToMany((type) => ItemInBasket, (entity) => entity.user)
+  itemsInBasket: ItemInBasket[];
+}
