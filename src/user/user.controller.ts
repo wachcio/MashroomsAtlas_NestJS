92df82c3b444362e9572912e6640f98922120cf9
wachcio <wachcio@poczta.fm>
2020-12-12 19:09:58 +0100

    Get one user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,32 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { RegisterUserResponse } from '../interfaces/user';
 import { UserService } from './user.service';
 import { RegisterDto } from './dto/register.dto';
+import { UserRoleAdminGuard } from 'src/guards/user-role-admin.guard';
+import { User } from './user.entity';
 
 @Controller('user')
 export class UserController {
   constructor(@Inject(UserService) private userService: UserService) {}
 
   @Post('/register')
+  @UseGuards(AuthGuard('jwt'), UserRoleAdminGuard)
   register(@Body() newUser: RegisterDto): Promise<RegisterUserResponse> {
     return this.userService.register(newUser);
+  }
+
+  @Get('/:id')
+  @UseGuards(AuthGuard('jwt'), UserRoleAdminGuard)
+  getOneUser(@Param('id') id: string): Promise<User> {
+    return this.userService.getOneUser(id);
   }
 }
