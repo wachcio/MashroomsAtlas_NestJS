e3d54e3aa1a6eadebc7208f5e6949d4fd864ce66
wachcio <wachcio@poczta.fm>
2020-12-13 14:40:21 +0100

    Add entity and dto

--- src/image/entities/image.entity.ts
+++ src/image/entities/image.entity.ts
@@ -1,1 +1,14 @@
-export class Image {}
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { ImageDto } from '../dto/image.dto';
+
+@Entity()
+export class Image extends BaseEntity implements ImageDto {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+  })
+  mushroomId: string;
+}
