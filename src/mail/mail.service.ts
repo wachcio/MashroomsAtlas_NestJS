4efe315091ba0cdb5ec9e2259b5bde93c1315ebc
wachcio <wachcio@poczta.fm>
2021-01-09 01:54:36 +0100

    fix small bug

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -6,8 +6,6 @@ export class MailService {
   constructor(protected readonly mailerService: MailerService) {}
 
   async sendMail(to: string, subject: string, html: string): Promise<any> {
-    console.log(to, subject, html);
-
     await this.mailerService.sendMail({
       to,
       subject,
