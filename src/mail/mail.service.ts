faeb92446ccbe6d4574c61635410b5b426c3480d
wachcio <wachcio@poczta.fm>
2021-01-11 01:19:10 +0100

    add and use infoToAdmin.hbs and change welcome.hbs

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -16,28 +16,23 @@ export class MailService {
 
   async sendWelcomeMessage({ username, email }: User): Promise<any> {
     const subject = `Rejestracja w Atlasie grzybów`;
-    // const html = `Witam.<BR>
-    // Zarejestrowałeś się w atlasie grzybów jako <b>${username}</b>. Na razie nie możesz edytować danych w nim zawartych. Jeśli chcesz to robić poproś o taką możliwość administrację serwisu.`;
+
     await this.mailerService.sendMail({
       to: email,
       subject,
       template: 'welcome',
-      context: { username, email },
+      context: { username, emailToAdmin: process.env.MAILER_ADDRESS_TO_ADMIN },
     });
   }
 
   async sendMessageToAdmin({ username, email, id }: User): Promise<any> {
     const subject = `Nowa rejestracja w Atlasie grzybów`;
-    const html = `Witam.<BR>
-    Zarejestrował się nowy użytkownik.<BR><BR>
-    Dane użytkownika:<br>
-    Nazwa: ${username}<br>
-    Email: ${email}<br>
-    ID: ${id}<br>`;
+
     await this.mailerService.sendMail({
       to: process.env.MAILER_ADDRESS_TO_ADMIN,
       subject,
-      html,
+      template: 'infoToAdmin',
+      context: { username, email, id },
     });
   }
 }
